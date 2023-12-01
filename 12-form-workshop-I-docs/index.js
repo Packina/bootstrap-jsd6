2e25d38297a785be5fdb8005b3823bae0b0c421d
Packina <136891046+Packina@users.noreply.github.com>
2023-12-01 08:20:46 +0700

    Move the alert button

--- 12-form-workshop-I-docs/index.js
+++ 12-form-workshop-I-docs/index.js
@@ -1,39 +1,40 @@
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
 
-    const appendAlert = (message, type, dismissible) => {
-        const wrapper = document.createElement('div');
-        const dismissibleClass = dismissible ? 'alert-dismissible' : '';
-        wrapper.innerHTML = [
-            `<div class="alert alert-${type} ${dismissibleClass}" role="alert">`,
-            `   <div>${message}</div>`,
-            dismissible ? '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' : '',
-            '</div>'
-        ].join('');
+const appendAlert = (message, type, dismissible) => {
+    const wrapper = document.createElement('div');
+    const dismissibleClass = dismissible ? 'alert-dismissible' : '';
 
-        alertPlaceholder.innerHTML = ''; 
-        alertPlaceholder.append(wrapper);
-    };
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} ${dismissibleClass}" role="alert">`,
+        `   <div>${message}</div>`,
+        dismissible ? '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' : '',
+        '</div>'
+    ].join('');
 
-    const alertTrigger = document.getElementById('liveAlertBtn');
-    if (alertTrigger) {
-        alertTrigger.addEventListener('click', () => {
-            if (!validateForm()) {
-                appendAlert('Please fill in all fields!', 'warning', true);
-            } else {
-                appendAlert('Thanks for filling in the information!', 'success', true);
-            }
-        });
-    }
+    alertPlaceholder.innerHTML = ''; 
+    alertPlaceholder.append(wrapper);
+};
 
-    function validateForm() {
-        var firstName = document.getElementById("firstName").value;
-        var lastName = document.getElementById("lastName").value;
-        var email = document.getElementById("email").value;
-        var password = document.getElementById("password").value;
-
-        if (firstName === "" || lastName === "" || email === "" || password === "") {
-            return false; // Fields are empty
+const alertTrigger = document.getElementById('liveAlertBtn');
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        if (!validateForm()) {
+            appendAlert('Please fill in all fields!', 'warning', true);
+        } else {
+            appendAlert('Thanks for filling in the information!', 'success', true);
         }
+    });
+}
 
-        return true; // All fields are filled
+function validateForm() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if (firstName === "" || lastName === "" || email === "" || password === "") {
+        return false; // Fields are empty
     }
+
+    return true; // All fields are filled
+}
